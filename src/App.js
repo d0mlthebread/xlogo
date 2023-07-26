93d3728b27d6089a160b5f0d5a45e90127eebd8a
d_bread <dom.fuss@gmail.com>
2023-07-26 13:52:10 +0200

    positioning

--- src/App.js
+++ src/App.js
@@ -39,15 +39,13 @@ function App() {
     canvas.height = canvasWidth;
 
     const ctx = canvas.getContext("2d");
-    const x = canvas.width / 2;
-    const y = canvas.width / 2.6;
     ctx.fillStyle = "#000";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = "#eff3f4";
     ctx.font = `${fontSize}px CustomFont`;
     ctx.textAlign = "center";
     ctx.textBaseline = "middle";
-    ctx.fillText(character, x, y);
+    ctx.fillText(character, canvas.width / 2, canvas.width / 2.66);
 
 
 
